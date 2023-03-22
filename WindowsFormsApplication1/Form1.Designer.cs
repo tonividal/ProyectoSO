45c69ca37033d09bf35023120095eb2e1a5c3599
tonividal <antoni.vidal.panella@estudiantat.upc.edu>
2023-03-22 08:45:22 +0100

    v1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.partidas = new System.Windows.Forms.RadioButton();
+            this.jugadresenestadio = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.estadioBox = new System.Windows.Forms.TextBox();
+            this.dosgoles = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.user = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(140, 47);
+            this.nombre.Margin = new System.Windows.Forms.Padding(4);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(217, 22);
+            this.nombre.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(30, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "connectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 190);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Enviar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.partidas);
+            this.groupBox1.Controls.Add(this.jugadresenestadio);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.estadioBox);
+            this.groupBox1.Controls.Add(this.dosgoles);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.nombre);
+            this.groupBox1.Location = new System.Drawing.Point(57, 303);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(522, 249);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Petició";
+            // 
+            // partidas
+            // 
+            this.partidas.AutoSize = true;
+            this.partidas.Location = new System.Drawing.Point(155, 91);
+            this.partidas.Margin = new System.Windows.Forms.Padding(4);
+            this.partidas.Name = "partidas";
+            this.partidas.Size = new System.Drawing.Size(278, 21);
+            this.partidas.TabIndex = 7;
+            this.partidas.TabStop = true;
+            this.partidas.Text = "Quines partides ha participat el jugador";
+            this.partidas.UseVisualStyleBackColor = true;
+            this.partidas.CheckedChanged += new System.EventHandler(this.Longitud_CheckedChanged);
+            // 
+            // jugadresenestadio
+            // 
+            this.jugadresenestadio.AutoSize = true;
+            this.jugadresenestadio.Location = new System.Drawing.Point(155, 134);
+            this.jugadresenestadio.Margin = new System.Windows.Forms.Padding(4);
+            this.jugadresenestadio.Name = "jugadresenestadio";
+            this.jugadresenestadio.Size = new System.Drawing.Size(304, 21);
+            this.jugadresenestadio.TabIndex = 7;
+            this.jugadresenestadio.TabStop = true;
+            this.jugadresenestadio.Text = "Mostra els jugadors que han jugat a l\'estadi";
+            this.jugadresenestadio.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 112);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Estadi";
+            // 
+            // estadioBox
+            // 
+            this.estadioBox.Location = new System.Drawing.Point(28, 133);
+            this.estadioBox.Margin = new System.Windows.Forms.Padding(4);
+            this.estadioBox.Name = "estadioBox";
+            this.estadioBox.Size = new System.Drawing.Size(81, 22);
+            this.estadioBox.TabIndex = 9;
+            // 
+            // dosgoles
+            // 
+            this.dosgoles.AutoSize = true;
+            this.dosgoles.Location = new System.Drawing.Point(155, 112);
+            this.dosgoles.Margin = new System.Windows.Forms.Padding(4);
+            this.dosgoles.Name = "dosgoles";
+            this.dosgoles.Size = new System.Drawing.Size(328, 21);
+            this.dosgoles.TabIndex = 8;
+            this.dosgoles.TabStop = true;
+            this.dosgoles.Text = "Jugadors amb 2 o + gols i l\'estadi on ho han fet";
+            this.dosgoles.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(237, 69);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 39);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "desconnectar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.password);
+            this.groupBox2.Controls.Add(this.user);
+            this.groupBox2.Location = new System.Drawing.Point(57, 126);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(521, 170);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log in";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(428, 130);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Entrar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(336, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Registrar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PASSWORD";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "USER";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(168, 91);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(152, 22);
+            this.password.TabIndex = 1;
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(126, 36);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(195, 22);
+            this.user.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 565);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton partidas;
+        private System.Windows.Forms.RadioButton dosgoles;
+        private System.Windows.Forms.RadioButton jugadresenestadio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox estadioBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+            
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string mensaje = "1/" + user.Text + "/" + password.Text;
+            // Enviamos al servidor el user y contraseña tecleados
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+            if (mensaje == "SI")
+                MessageBox.Show("Ok");
+            else
+                MessageBox.Show("credencials incorrectes.");
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (partidas.Checked)
+            {
+                string mensaje = "3/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split ('\0')[0];
+                MessageBox.Show("ID de les partides on ha participat: " + mensaje);
+            }
+            else if (dosgoles.Checked)
+            {
+                string mensaje = "4/" ;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show("Jugadors amb més de dos gols i l'estadi: " + mensaje);
+
+               /* if (mensaje == "SI")
+                    MessageBox.Show("Tu nombre ES bonito.");
+                else
+                    MessageBox.Show("Tu nombre NO bonito. Lo siento.");*/
+
+            }
+            else if (jugadresenestadio.Checked)
+            {
+                // Enviamos nombre y altura
+                string mensaje = "5/" + estadioBox.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show("Jugadors que han jugat allà: " +mensaje);
+            }
+             
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Mensaje de desconexión
+            string mensaje = "0/";
+        
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            // Nos desconectamos
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+
+
+        }
+
+        private void Longitud_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
