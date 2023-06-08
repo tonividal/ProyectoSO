26165ed3a8a05a9c5bccaff198a38f34e6dae0b6
tonividal <antoni.vidal.panella@estudiantat.upc.edu>
2023-06-08 18:10:27 +0200

    base grafica

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApplication1
     {
         Socket server;
         Thread atender;
-        int port = 9070;
+        int port = 9020;
         int color;
         List<string> pdas = new List<string>();
 
@@ -123,13 +123,13 @@ namespace WindowsFormsApplication1
                         DataTable dt = new DataTable();
                         dt.Columns.Add("Conectados", typeof(string));
 
-
-                        for (int i = 0; i < trozos.Length - 2; i++)
+                        int i = 0;
+                        for (i = 0; i < trozos.Length - 2; i++)
                         {
                             trozos[i] = trozos[i + 1];
                         }
 
-
+                        Array.Resize(ref trozos, i);
 
                         foreach (string nombre in trozos)
                         {
@@ -168,7 +168,7 @@ namespace WindowsFormsApplication1
                            dataGridView1.Rows[i].Cells[0].Value = listaconectados[i];
 
                        }*/
-                       break;
+                        break;
 
 
 
